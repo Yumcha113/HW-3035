1746fd2f9c5e40a096f396eaf668b478219d297d
Andy <atlimm22@gmail.com>
2026-02-16 15:19:25 -0800

    Adding coding files

---
+++ lab9.py
@@ -0,0 +1,29 @@
+import math
+
+# 1.
+def findElement(input_string, index):
+    return input_string[index]
+
+# 2.
+def concatStrings(string1, string2):
+    return string1 + string2
+
+# 3.
+def divideString(input_string):
+    half_length = math.floor(len(input_string) / 2)
+    return input_string[0:half_length]
+
+# 4.
+def findSubstring(string1, string2):
+    return string2 in string1
+
+# Test code
+inputString1 = "This is a test string"
+inputString2 = "...it tests your functions"
+inputString3 = "functions"
+
+print(findElement(inputString1, 0))
+print(concatStrings(inputString1, inputString2))
+print(divideString(inputString1))
+print(findSubstring(inputString2, inputString3))
+print(findSubstring(inputString1, inputString3))
